353fd7ea577dc794f4e0cddd9ec8a86a48df284e
sunyong186 <sunyong1860@gmail.com>
2025-09-30 00:34:40 +0900

    week3 TabLayout + Viewpager2 구현 / 추가 UI 구현

--- UMC_Android_Mission/app/src/main/java/com/example/umc_android_mission2/AlbumFragment.kt
+++ UMC_Android_Mission/app/src/main/java/com/example/umc_android_mission2/AlbumFragment.kt
@@ -6,16 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.umc_android_mission2.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
 
     private var _binding: FragmentAlbumBinding? = null
     private val binding get() = _binding!!
 
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
+        binding.albumBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, HomeFragment())
+                .commitAllowingStateLoss()
+        }
+
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) {
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
+        //.attach()는 탭레이아웃과 뷰페이저2를 붙이는 메서드
+
         return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- UMC_Android_Mission/app/src/main/java/com/example/umc_android_mission2/LockerFragment.kt
+++ UMC_Android_Mission/app/src/main/java/com/example/umc_android_mission2/LockerFragment.kt
@@ -5,16 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.umc_android_mission2.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LockerFragment : Fragment() {
+
+    private var _binding: FragmentLockerBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val information = arrayListOf("저장한 곡", "음악파일","저장앨범")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // fragment_home.xml 레이아웃을 inflate해서 반환
-        return inflater.inflate(R.layout.fragment_locker, container, false)
-    }
 
+        _binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        val lockerVPAdapter = LockerVPAdapter(this)
+        binding.lockerContentVp.adapter = lockerVPAdapter
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp) { tab, position ->
+            tab.text = information[position]
+        }.attach()
+
+        return binding.root
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
